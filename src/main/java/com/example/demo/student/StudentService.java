69d0c98af390801dd3ab659464100fd8bfa1e711
Vikram <vbedi@expediagroup.com>
2021-04-12 17:26:24 +0530

    Added put req

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -44,6 +44,7 @@ public class StudentService {
     @Transactional
     public void updateStudent(Long studentId, String name, String email) {
         // By using Transactional we just use the setters for our class and it will update the DB too
+        // Entity went into managed state because of this
         Student student=studentRepository.findById(studentId).orElseThrow(()-> new IllegalStateException("Student with id "+studentId+" doesn't exist"));
 
         if(name!=null && name.length()>0 && !Objects.equals(student.getName(),name)){
