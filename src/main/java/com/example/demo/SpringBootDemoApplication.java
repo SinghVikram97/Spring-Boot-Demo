e1ba66801812e9406b246b07604e8f36a67f4efd
Vikram <vbedi@expediagroup.com>
2021-04-12 10:44:30 +0530

    Added simple get req for student list

--- src/main/java/com/example/demo/SpringBootDemoApplication.java
+++ src/main/java/com/example/demo/SpringBootDemoApplication.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
 @RestController // Makes the class serve rest endpoints
@@ -14,8 +19,14 @@ public class SpringBootDemoApplication {
     }
 
     @GetMapping // To specify Request, this is Get request
-    public String hello(){
-        return "Hello World";
+    public List<Student> hello(){
+        return List.of(
+                new Student(1L,
+                        "Mariam",
+                        "marian.jamal@gmail.com",
+                        LocalDate.of(2000, Month.JANUARY,5),
+                        21)
+        );
     }
 
 }
