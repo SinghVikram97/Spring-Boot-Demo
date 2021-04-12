2163a404f27c9557432b6adefb9625e8cba8c202
Vikram <vbedi@expediagroup.com>
2021-04-12 12:45:34 +0530

    Mapped student class to a table in database

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,8 +1,22 @@
 package com.example.demo.student;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity // @Entity annotation defines that a class can be mapped to a table.
+@Table // Specifies that it should be mapped to a table
 public class Student {
+
+    @Id
+    @SequenceGenerator(
+            name="student_sequence",
+            sequenceName="student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String email;
