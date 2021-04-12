00ad9fc516aa6d46b1165f4477692f8dd860d4dc
Vikram <vbedi@expediagroup.com>
2021-04-12 17:22:36 +0530

    Added put request

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -2,8 +2,10 @@ package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -37,5 +39,25 @@ public class StudentService {
             throw new IllegalStateException("Student with id "+studentId+" doesn't exist");
         }
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        // By using Transactional we just use the setters for our class and it will update the DB too
+        Student student=studentRepository.findById(studentId).orElseThrow(()-> new IllegalStateException("Student with id "+studentId+" doesn't exist"));
+
+        if(name!=null && name.length()>0 && !Objects.equals(student.getName(),name)){
+            student.setName(name);
+        }
+
+        if(email!=null && email.length()>0 && !Objects.equals(student.getEmail(),email)){
+            // Here we also check if email hasn't been taken by some other user
+            Optional<Student> studentOptional=studentRepository.findStudentByEmail(email);
+            if(studentOptional.isPresent()){
+                throw new IllegalStateException("Email taken");
+            }
+            student.setEmail(email);
+        }
+
     }
 }
